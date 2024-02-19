afc0e38f8ac6ab986ef284bfe338a1aa65fa638b
maryan <12maxmed@gmail.com>
2024-02-19 19:05:20 +0000

    made fake database

--- ATM.cs
+++ ATM.cs
@@ -2,59 +2,59 @@ using System ;
 
 public class cardHolder{
     // OOP
-    String cardNumber;
+    String CardNumber;
     int pin;
-    String firstName;
-    String lastName;
+    String FirstName;
+    String LastName;
     double balance;
 
     // initalise constructor
-    public cardHolder(String cardNumber, int pin, String firstName, String lastName, double balance){
-    this.cardNumber = cardNumber;
+    public cardHolder(String CardNumber, int pin, String FirstName, String LastName, double balance){
+    this.CardNumber = CardNumber;
     this.pin = pin;
-    this.firstName = firstName;
-    this.lastName = lastName;
+    this.FirstName = FirstName;
+    this.LastName = LastName;
     this.balance = balance;
     }
 
     // GETTERS AND SETTERS
-    public String getCardNumber(){
-        return cardNumber;
+    public String GetCardNumber(){
+        return CardNumber;
     }
 
-    public int getPin(){
+    public int GetPin(){
         return pin;
     }
 
-    public String getFirstName(){
-        return firstName;
+    public String GetFirstName(){
+        return FirstName;
     }
     
-    public String getLastName(){
-        return lastName;
+    public String GetLastName(){
+        return LastName;
     }
 
-    public double getBalance(){
+    public double GetBalance(){
         return balance;
     }
 
-    public String setCardNumber(){
-        cardNumber = newCardNumber;
+    public String SetCardNumber(){
+        CardNumber = newCardNumber;
     }
 
-    public int setPin(){
+    public int SetPin(){
         pin = newPin;
     }
 
-    public String setFirstName(){
-        firstName = newFirstName;
+    public String SetFirstName(){
+        FirstName = newFirstName;
     }
     
-    public String setLastName(){
-        lastName = newLastName
+    public String SetLastName(){
+        LastName = newLastName
     }
 
-    public double setBalance(){
+    public double SetBalance(){
         balance = newBalance;
     }
 
@@ -71,11 +71,38 @@ public class cardHolder{
 
         // this method will handle deposit
         void deposit(cardHolder currentUser){
-            Console.WriteLine("How much would you like to deposit? ");
+            Console.WriteLine("How much would you like to deposit?: ");
             double deposit = Double.parse(Console.ReadLine());
             // console.readline will read what the user has inputted.
-            currentUser.setBalance(deposit);
+            currentUser.SetBalance(deposit);
+            Console.WriteLine("Thank you for your money. Your new balance is: " + currentUser.GetBalance());
         }
+
+        // this method will handle withdrawals
+
+        void withdrawal(cardHolder currentUser){
+            Console.WriteLine("How much would you like to withdraw?: ");
+            double withdraw = Double.parse(Console.ReadLine());
+            // need to check if the user has enough money to withdraw
+            if(currentUser.GetBalance() > withdraw){
+                currentUser.SetBalance(currentUser.GetBalance() - withdraw);
+            } else {
+                Console.WriteLine("Insufficent funds. You ain't got no money!")
+            }
+            Console.WriteLine("Thank you. Your new balance is: " + currentUser.GetBalance());
+        }
+
+        // return the balance of the user 
+        void balance(cardHolder currentUser){
+            Console.WriteLine("Current balance: " + currentUser.GetBalance());
+        }
+
+        // create fake accounts to test app.
+        List<cardHolder> ListOfCardHolders = new List<cardHolder>();
+        ListOfCardHolders.add(new cardHolder("123", 123, "John", "Smith", 130.50));
+        ListOfCardHolders.add(new cardHolder("456", 456, "Jane", "Doe", 200.10));
+        ListOfCardHolders.add(new cardHolder("789", 789, "Micheal", "Jackson", 130000.59));
+        ListOfCardHolders.add(new cardHolder("321", 321, "Eren", "Yeager", 1.05));
 
     }
 
